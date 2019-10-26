359fb21c33cd10b8ebf9b5e129942d7fb7542aa2
Anna Goncharova <anna.s.goncharova.2001@gmail.com>
2019-10-26 14:37:52 +0300

    init

--- InternetShop/src/com/uits/Store.java
+++
@@ -1,9 +0,0 @@
-package com.uits.task13;
-
-public class Store  {
-    String title;
-    double price;
-    String store;
-
-
-}
--- InternetShop/src/com/uits/Store_UI.java
+++
@@ -1,9 +0,0 @@
-package com.uits.task13;
-
-import javax.swing.*;
-
-public class Store_UI {
-    private JCheckBox checkBox1;
-    private JTextField textField1;
-    private JTextField textField2;
-}
