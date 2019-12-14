6bc3c1253924db3cf5054878581cb4ab75b524a0
Anna Goncharova <anna.s.goncharova.2001@gmail.com>
2019-12-14 17:53:02 +0200

    authentication, way to file inited

---
+++ Semesters3Task/src/com/company/Lingvist.java
@@ -0,0 +1,33 @@
+package com.company;
+import java.io.File;
+import java.io.IOException;
+
+public class Lingvist {
+    public static void main( String[] args ) {
+        System.out.print("Добро пожаловать! Чтобы провести транслитерацию имен собсвенных  выбырете таблицу");
+
+        File myFile = new File("/home/anna/TranslitTables");
+        System.out.println("File name: " + myFile.getName());
+        System.out.println("Parent folder: " + myFile.getParent());
+        if(myFile.exists())
+            System.out.println("File exists");
+        else
+            System.out.println("File not found");
+
+        System.out.println("File size: " + myFile.length());
+        if(myFile.canRead())
+            System.out.println("File can be read");
+        else
+            System.out.println("File can not be read");
+
+        if(myFile.canWrite())
+            System.out.println("File can be written");
+        else
+            System.out.println("File can not be written");
+
+
+    }
+}
+
+    }
+}
