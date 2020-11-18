b3c9d2392483389b773da3319c6bcbb5ea26af79
Anna Goncharova <anna.s.goncharova.2001@gmail.com>
2020-11-18 20:21:26 +0200

    first commit arrays

---
+++ NIXinternship1/src/myExamples/arrays/WeekDays.java
@@ -0,0 +1,18 @@
+package myExamples.arrays;
+
+import java.util.Arrays;
+
+public class WeekDays {
+    public static void main( String[] args ) {
+
+
+
+        String[] days = new String[7];
+
+        days= new String[]{"Monday", "Tuesday ", " Wednesday", "Thursday ", " Friday", "Saturday ", " Sunday"};
+
+        System.out.println(Arrays.toString(days));
+
+
+    }
+}
